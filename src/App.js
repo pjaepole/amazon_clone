4dd2d5f869284a7af2d0738a91fd4d349a751179
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-01-03 19:21:53 -0800

    header component created and exported to app.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Homepage from './components/Homepage';
 import Checkout from './components/Checkout';
 import Login from './components/Login';
+import Header from './components/Header';
 function App() {
   return (
     <Router>
-      
+        <Header></Header>
         <Routes>
-          <Route path='/checkout'element={<Checkout/>}/>
-            
-          
-          <Route path='/login'element={<Login/>}/>
-            
-          
+          <Route path='/checkout' element={<Checkout/>}/>          
+          <Route path='/login' element={<Login/>}/> 
           <Route path='/' element={<Homepage/>}/>
-            
-          
         </Routes>
-      
     </Router>
   );
 }
