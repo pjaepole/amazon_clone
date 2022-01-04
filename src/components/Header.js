67282a119f1534a3ffc551d60c73031f0b57b5e6
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-01-03 20:00:49 -0800

    imported Link from reactrouterdom in header.js and linked it to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import amazlogo from '../amazon_PNG11.png'
 import './Header.css'
+import amazlogo from '../amazon_PNG11.png'
+import { Link } from 'react-router-dom'
 function Header(){
     return (
         <nav className='header'>
-            <img className='header_logo' alt='amazon-logo'src={amazlogo} />
+            <Link to='/'>
+                <img className='header_logo' alt='amazon-logo'src={amazlogo} />
+            </Link>
         </nav>
     )
 }
