5db3a0ab3a96d6e7ac288c10ed26578602824701
Jaehun ParkLaptop <pjaehun4122@gmail.com>
2022-01-04 23:20:53 -0800

    done for the home component, added arrow buttons to change image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,45 @@
+import './Home.css'
 import {snapple} from './Picture'
 import React, {useState} from 'react'
 function Home(){
-    const [pictures,setPictures]=useState(snapple)
-    console.log(pictures)
+    const leftarrow=()=>{
+        if(picindex===0){
+            setPicindex(3)
+        } else {
+            setPicindex(picindex-1)
+        }
+    }
+    const rightarrow=()=>{
+        if(picindex===3){
+            setPicindex(0)
+        } else {
+            setPicindex(picindex+1)
+        }
+    }
+    const [picindex,setPicindex]=useState(0)
     return (
-        <div className='home'>
-            <img src={pictures[0]}/>
+        <div  className='home'>
+            <div className='home_background_img'
+                style={
+                    {background: `url(${snapple[picindex]})`}
+            }>
+
+             <div className='home_leftarrow' onClick={leftarrow}>&lt;</div>   
+             <div className='home_rightarrow' onClick={rightarrow}>&#62;</div>   
+            </div> 
+            
         </div>
     )
 }
 
 export default Home
+
+  // ,
+                    // border:"solid orange",
+                    // backgroundSize:"cover",
+                    // backgroundRepeat:"no-repeat",
+                    // height:"45vh",
+                    // display:"flex",
+                    // justifyContent:'space-between',
+                    // alignItems:'center',
+                    // fontSize:'45px'
